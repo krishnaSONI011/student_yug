b8a6b7f59d39170e5215a83ee9862cc2f0eed578
krishnaSONI011 <soni01krishna@gmail.com>
2025-12-12 18:45:28 +0530

    hy

--- app/help-center/page.tsx
+++ app/help-center/page.tsx
@@ -9,10 +9,10 @@ export default function HelpCenter(){
           </div>
 
 
-        <div className="px-2 py-2  mb-10">
-            <p className="mt-10  text-lg text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet vero in eos voluptates, voluptatibus velit consequatur tenetur error, illum excepturi sint ut! Sint unde adipisci odit doloribus commodi assumenda facilis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ipsam eum enim quisquam accusantium eligendi a! Illum possimus nobis, sunt doloribus quidem sed ab molestiae est harum unde fuga vel.</p>
-            <p className="mt-10 text-lg text-justify ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet vero in eos voluptates, voluptatibus velit consequatur tenetur error, illum excepturi sint ut! Sint unde adipisci odit doloribus commodi assumenda facilis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ipsam eum enim quisquam accusantium eligendi a! Illum possimus nobis, sunt doloribus quidem sed ab molestiae est harum unde fuga vel.</p>
-            <p className="mt-10 text-lg text-justify ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet vero in eos voluptates, voluptatibus velit consequatur tenetur error, illum excepturi sint ut! Sint unde adipisci odit doloribus commodi assumenda facilis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ipsam eum enim quisquam accusantium eligendi a! Illum possimus nobis, sunt doloribus quidem sed ab molestiae est harum unde fuga vel.</p>
+        <div className="px-2 py-2 ` mb-10">
+            <p className="mt-10 px-10  text-lg text-justify">Welcome to the StudentYug Help Center — your dedicated space to find quick answers, clear guidance, and reliable support. Whether you re exploring courses, managing your profile, or seeking information about academic tools, our goal is to make your journey smoother and stress-free. This help center is designed to guide you step-by-step so you never feel stuck while using our platform.</p>
+            <p className="mt-10 px-10   text-lg text-justify ">From common questions to detailed how-to instructions, you’ll find resources that help you understand every feature of StudentYug. We’ve organized the information in an easy-to-navigate format, so you can locate exactly what you need within seconds. If you’re new to the platform, the help center also offers essential tips to help you get started confidently.</p>
+            <p className="mt-10 px-10   text-lg text-justify ">If you ever need additional assistance, our support team is always ready to help. You can reach out for personalized support, share your concerns, or request guidance on any issue you face. At StudentYug, your experience matters — and we’re committed to ensuring you enjoy a smooth, helpful, and empowering learning journey.</p>
             
 
         </div>
