d75c615aab0fb31d43a13097c18b0ae80db48394
krishnaSONI011 <soni01krishna@gmail.com>
2025-12-12 16:31:03 +0530

    ss

--- app/help-center/page.tsx
+++ app/help-center/page.tsx
@@ -5,12 +5,12 @@ export default function HelpCenter(){
     return(
         <>
           <div className="flex justify-center items-center h-[40vh] bg-gradient-to-br from-[#204b74] via-[#204b74] to-[#204b74] text-white relative overflow-hidden">
-                    <h1 className="text-5xl md:text-7xl font-bold mb-6 leading-tight">HELP CENTER</h1>
+                    <h1 className="text-5xl md:text-7xl font-bold mb-6 leading-tight">Help Center</h1>
           </div>
 
 
-        <div className="px-2 py-2 mb-10">
-            <p className="mt-10 text-lg text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet vero in eos voluptates, voluptatibus velit consequatur tenetur error, illum excepturi sint ut! Sint unde adipisci odit doloribus commodi assumenda facilis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ipsam eum enim quisquam accusantium eligendi a! Illum possimus nobis, sunt doloribus quidem sed ab molestiae est harum unde fuga vel.</p>
+        <div className="px-2 py-2  mb-10">
+            <p className="mt-10  text-lg text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet vero in eos voluptates, voluptatibus velit consequatur tenetur error, illum excepturi sint ut! Sint unde adipisci odit doloribus commodi assumenda facilis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ipsam eum enim quisquam accusantium eligendi a! Illum possimus nobis, sunt doloribus quidem sed ab molestiae est harum unde fuga vel.</p>
             <p className="mt-10 text-lg text-justify ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet vero in eos voluptates, voluptatibus velit consequatur tenetur error, illum excepturi sint ut! Sint unde adipisci odit doloribus commodi assumenda facilis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ipsam eum enim quisquam accusantium eligendi a! Illum possimus nobis, sunt doloribus quidem sed ab molestiae est harum unde fuga vel.</p>
             <p className="mt-10 text-lg text-justify ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet vero in eos voluptates, voluptatibus velit consequatur tenetur error, illum excepturi sint ut! Sint unde adipisci odit doloribus commodi assumenda facilis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ipsam eum enim quisquam accusantium eligendi a! Illum possimus nobis, sunt doloribus quidem sed ab molestiae est harum unde fuga vel.</p>
             
